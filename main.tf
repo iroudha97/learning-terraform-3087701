048023d386615567548dd2d477ac0e908b3b508c
iroudha97 <iroudha97@gmail.com>
2024-11-26 08:38:33 +0400

    fix3

--- main.tf
+++ main.tf
@@ -53,6 +53,8 @@ module "alb" {
 
   name    = "blog-alb"
 
+  load_balancer_type = "application"
+  
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
